4fcd6cd6baa4cf9f64e1e7dfd0301f8cf453f628
Spooky Kipper <88220209+SpookyKipper@users.noreply.github.com>
2025-09-23 21:18:23 +0800

    Shortcut modify page button

--- src/base/options.js
+++ src/base/options.js
@@ -1,17 +1,27 @@
 // Saves options to chrome.storage
 const saveOptions = () => {
-  const auto_disband_group = document.getElementById('auto_disband_group').checked;
-  const auto_created_group_name = document.getElementById('auto_created_group_name').value;
-  const auto_created_group_name_search_engine = document.getElementById('auto_created_group_name_search_engine').value;
+  const auto_disband_group =
+    document.getElementById("auto_disband_group").checked;
+  const auto_created_group_name = document.getElementById(
+    "auto_created_group_name"
+  ).value;
+  const auto_created_group_name_search_engine = document.getElementById(
+    "auto_created_group_name_search_engine"
+  ).value;
 
   chrome.storage.sync.set(
-    { auto_disband_group: auto_disband_group, auto_created_group_name: auto_created_group_name, auto_created_group_name_search_engine: auto_created_group_name_search_engine},
+    {
+      auto_disband_group: auto_disband_group,
+      auto_created_group_name: auto_created_group_name,
+      auto_created_group_name_search_engine:
+        auto_created_group_name_search_engine,
+    },
     () => {
       // Update status to let user know options were saved.
-      const status = document.getElementById('status');
-      status.textContent = 'Options saved.';
+      const status = document.getElementById("status");
+      status.textContent = "Options saved.";
       setTimeout(() => {
-        status.textContent = '';
+        status.textContent = "";
       }, 750);
     }
   );
@@ -21,14 +31,31 @@ const saveOptions = () => {
 // stored in chrome.storage.
 const restoreOptions = () => {
   chrome.storage.sync.get(
-    {auto_disband_group: true, auto_created_group_name: "%domain%", auto_created_group_name_search_engine: "%search_query%"},
+    {
+      auto_disband_group: true,
+      auto_created_group_name: "%domain%",
+      auto_created_group_name_search_engine: "%search_query%",
+    },
     (items) => {
-        document.getElementById('auto_disband_group').checked = items.auto_disband_group;
-        document.getElementById('auto_created_group_name').value = items.auto_created_group_name;
-        document.getElementById('auto_created_group_name_search_engine').value = items.auto_created_group_name_search_engine;
+      document.getElementById("auto_disband_group").checked =
+        items.auto_disband_group;
+      document.getElementById("auto_created_group_name").value =
+        items.auto_created_group_name;
+      document.getElementById("auto_created_group_name_search_engine").value =
+        items.auto_created_group_name_search_engine;
     }
   );
 };
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.getElementById('save').addEventListener('click', saveOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
+
+document
+  .getElementById("openKeyboardShortcut")
+  .addEventListener("click", () => {
+    if (typeof browser == "undefined") {
+      chrome.tabs.create({ url: "chrome://extensions/shortcuts#:~:text=New tab in Group / Create a new group" });
+    } else {
+      browser.commands.openShortcutSettings();
+    }
+  });
